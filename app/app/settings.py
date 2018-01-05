3c267bff841bfdec17f25d7d4f9ce02657e80c79
Petteri Nevavuori <petteri.nevavuori@gmail.com>
2018-01-05 14:46:32 +0200

    Attempt at solving the problem of staticfiles not being served via uWSGI.

--- app/app/settings.py
+++ app/app/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = (
+    STATIC_ROOT,
+)
 
 # LinkedIn API
 LINKEDIN_AUTH_URL = 'https://www.linkedin.com/oauth/v2/authorization'
--- app/app/urls.py
+++ app/app/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from markdownx import urls as markdownx
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^markdownx/', include(markdownx)),
     url(r'^', include('homepage.urls', namespace='homepage')),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
